6610877b746fa6895dd4d71e31da87e146600e35
moonflower <tayler.brianne.baldwin@gmail.com>
2024-05-05 15:03:45 -0400

    add mutation and queries

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,33 +1,43 @@
-const typeDefs = `
+// import setup for Apollo Server with Express
+const { gql } = require('apollo-server-express');
 
-type  {
-    gameID: String
-   title: String!
-}
-
-type User {
-    _id: ID!
-    username: String!
-    email: String
-    trackedGames: [game]
-  }
-
-  type Auth {
-    token: ID!
-    user: User
-  }
-
-  
-  type Query {
-    me: User
-  }
-
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(title: String!, gameID: String): User
-    removeBook(title: String!): User
-  }
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    input InputBook {
+        bookId: String
+        authors: [String]
+        title: String
+        description: String
+        image: String
+        link: String
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(newBook: InputBook!): User
+        removeBook(bookId: ID!): User
+    }
 `;
 
 module.exports = typeDefs;
